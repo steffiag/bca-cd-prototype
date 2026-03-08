29beece3077aaef7abd94428f1a25024256aaaf4
steffiag <steffiageorge@gmail.com>
2026-03-07 22:58:47 -0500

    change

--- app/backend/server.js
+++ app/backend/server.js
@@ -985,10 +985,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "../dist")));
-app.get("/:path(.*)", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
-
 db.sequelize
   .sync()
   .then(() => {
