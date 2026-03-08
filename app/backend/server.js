c5253426ba5aa3e6892b87129d9a9bb48334538f
steffiag <steffiageorge@gmail.com>
2026-03-07 21:01:41 -0500

    change

--- app/backend/server.js
+++ app/backend/server.js
@@ -986,7 +986,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "../dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
